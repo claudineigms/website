ea9cfc765d66e58abb40e70cce48d906763a82b0
Claudinei Gomes <claudineigomes8@hotmail.com>
2022-03-18 21:44:54 -0300

    Atualização

--- rascunho.js
+++ rascunho.js
@@ -1,4 +1,12 @@
 function mudatema(){
+    var botao = document.getElementById("botao_tema")
+    if (botao.value == "dark"){
+       botao.innerHTML= "Alterar Tema<br>Dark"
+       botao.value = "white" 
+    }
+    else{botao.innerHTML= "Alterar Tema<br>White"
+        botao.value = "dark"}
+
     document.body.classList.toggle("white");
 
 }
